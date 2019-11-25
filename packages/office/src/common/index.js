4a46c8a6e18f533d218e2718c48850cd388469de
liuchunfeng <liuchunfeng@didiglobal.com>
2019-11-25 21:00:15 +0800

    update

--- packages/office/src/common/index.js
+++ packages/office/src/common/index.js
@@ -3,3 +3,12 @@ import moment from 'moment'
 moment.locale('zh-cn')
 
 export { moment }
+
+export const TYPE_NAME = {
+  all: '全部',
+  good: "精华",
+  share: '分享',
+  ask: '问答',
+  job: '招聘',
+  dev: '客户端测试'
+}
